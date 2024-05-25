1b20ab51ed69a7fbc382d30f87d1754cfb834288
l1keafox <raymond.ed.lewis@gmail.com>
2024-05-24 19:09:32 -0600

    Added MongoDB Models and Connect String
    Created Book and BookStoreDatabaseSettings Models and added appsettings database string

--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,12 @@
+using BookStoreApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Add services to the container.
+builder.Services.Configure<BookStoreDatabaseSettings>(
+builder.Configuration.GetSection("BookStoreDatabase"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,3 +29,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
