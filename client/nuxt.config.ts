02fc7a3c2ff08fb9ba9b87ce7c2f5e5aa0744a50
l1keafox <raymond.ed.lewis@gmail.com>
2024-05-28 16:50:12 -0600

    Add Tailwindcss
    Pnpm add tailwindcss and it's dependancies

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,10 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
   modules: [
     '@nuxtjs/storybook',
     '@vueuse/nuxt',
     '@pinia/nuxt'
   ],
-
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
