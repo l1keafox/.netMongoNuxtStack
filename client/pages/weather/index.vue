352d813f4fada17b57f5e33810c17549139487cc
l1keafox <raymond.ed.lewis@gmail.com>
2024-05-28 10:44:38 -0600

    Display Pinia Store
    displayed pinia data in weather route

--- client/pages/weather/index.vue
+++ client/pages/weather/index.vue
@@ -1,12 +1,13 @@
 <template>
   <h1>Weather</h1>
   <p>{{ data }}!</p>
+  <p>{{ store.count }}</p>
 </template>
 
 <script setup lang="ts">
-//import { useAlertsStore } from '@/composables/useCounterStore';
-//const store = useAlertsStore();
+import { useAlertsStore } from '@/composables/useCounterStore';
+const store = useAlertsStore();
 const {data} = await useFetch('http://localhost:5011/WeatherForecast');
-//console.log(store.count);
+console.log(store.count);
 console.log(data.value);
 </script>
